22197e7e4195a47d95896617d7e6adc4159ba1e8
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-05-15 16:16:47 +0700

    maybe this one?

--- database/factories/ProgramFactory.php
+++ database/factories/ProgramFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\program>
  */
-class ProgramFactory extends Factory
+class programFactory extends Factory
 {
     /**
      * Define the model's default state.
