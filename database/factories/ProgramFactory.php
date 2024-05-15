1a7a77ea282fe39adcb81e7196a22c5e0275a51e
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-05-15 16:40:16 +0700

    maybe the wrong one is the yaml?

--- database/factories/ProgramFactory.php
+++ database/factories/ProgramFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\program;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,7 +9,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class programFactory extends Factory
 {
-    protected $model = program::class;
     /**
      * Define the model's default state.
      *
