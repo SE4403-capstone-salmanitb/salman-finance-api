282269cfef469a83beb943f638323bf32f2d2556
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-05-15 16:27:04 +0700

    maybe now work, i add model in factory

--- database/factories/ProgramFactory.php
+++ database/factories/ProgramFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\program;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class programFactory extends Factory
 {
+    protected $model = program::class;
     /**
      * Define the model's default state.
      *
