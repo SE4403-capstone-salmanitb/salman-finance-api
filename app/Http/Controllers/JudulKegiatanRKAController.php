d5f4477b472305422302824fae2145f9a2a3f33b
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-05-16 09:58:59 +0700

    make file for judul kegiatan rka

---
+++ app/Http/Controllers/JudulKegiatanRKAController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JudulKegiatanRKA;
+use App\Http\Requests\StoreJudulKegiatanRKARequest;
+use App\Http\Requests\UpdateJudulKegiatanRKARequest;
+
+class JudulKegiatanRKAController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJudulKegiatanRKARequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JudulKegiatanRKA $judulKegiatanRKA)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJudulKegiatanRKARequest $request, JudulKegiatanRKA $judulKegiatanRKA)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JudulKegiatanRKA $judulKegiatanRKA)
+    {
+        //
+    }
+}
