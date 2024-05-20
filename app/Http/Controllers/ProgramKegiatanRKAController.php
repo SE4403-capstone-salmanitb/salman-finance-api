cc3ce093b350ace8841bbb9a6a9f54f6300953dd
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-05-20 12:51:20 +0700

    rka view

--- app/Http/Controllers/ProgramKegiatanRKAController.php
+++ app/Http/Controllers/ProgramKegiatanRKAController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\ProgramKegiatanRKA;
 use App\Http\Requests\StoreProgramKegiatanRKARequest;
 use App\Http\Requests\UpdateProgramKegiatanRKARequest;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
 
 class ProgramKegiatanRKAController extends Controller
@@ -65,5 +66,28 @@ class ProgramKegiatanRKAController extends Controller
         $programKegiatanRKA->delete();
 
         return response()->noContent();
+    }
+
+    /**
+     * Get the full table view of Program Kegiatan RKA
+     */
+    public function full(int $year)
+    {
+        $result = DB::table('program_kegiatan_r_k_a_s')
+        ->join('judul_kegiatan_r_k_a_s', 'program_kegiatan_r_k_a_s.id', '=', 'judul_kegiatan_r_k_a_s.id_program_kegiatan_rka')
+        ->join('item_kegiatan_r_k_a_s', 'judul_kegiatan_r_k_a_s.id', '=', 'item_kegiatan_r_k_a_s.id_judul_kegiatan')
+        ->select('program_kegiatan_r_k_a_s.nama', 'program_kegiatan_r_k_a_s.deskripsi', 'program_kegiatan_r_k_a_s.output',
+            DB::raw('SUM(CASE WHEN item_kegiatan_r_k_a_s.sumber_dana = "Pusat" THEN (item_kegiatan_r_k_a_s.nilai_satuan * item_kegiatan_r_k_a_s.quantity * item_kegiatan_r_k_a_s.frequency) ELSE 0 END) AS Dana_Pusat'),
+            DB::raw('SUM(CASE WHEN item_kegiatan_r_k_a_s.sumber_dana = "RAS" THEN (item_kegiatan_r_k_a_s.nilai_satuan * item_kegiatan_r_k_a_s.quantity * item_kegiatan_r_k_a_s.frequency) ELSE 0 END) AS Dana_RAS'),
+            DB::raw('SUM(CASE WHEN item_kegiatan_r_k_a_s.sumber_dana = "Kepesertaan" THEN (item_kegiatan_r_k_a_s.nilai_satuan * item_kegiatan_r_k_a_s.quantity * item_kegiatan_r_k_a_s.frequency) ELSE 0 END) AS Dana_Kepesertaan'),
+            DB::raw('SUM(CASE WHEN item_kegiatan_r_k_a_s.sumber_dana = "Pihak Ketiga" THEN (item_kegiatan_r_k_a_s.nilai_satuan * item_kegiatan_r_k_a_s.quantity * item_kegiatan_r_k_a_s.frequency) ELSE 0 END) AS Dana_Pihak_Ketiga'),
+            DB::raw('SUM(CASE WHEN item_kegiatan_r_k_a_s.sumber_dana = "Wakaf Salman" THEN (item_kegiatan_r_k_a_s.nilai_satuan * item_kegiatan_r_k_a_s.quantity * item_kegiatan_r_k_a_s.frequency) ELSE 0 END) AS Dana_Wakaf_Salman'),
+            DB::raw('SUM(item_kegiatan_r_k_a_s.nilai_satuan * item_kegiatan_r_k_a_s.quantity * item_kegiatan_r_k_a_s.frequency) AS Total_Dana')
+        )
+        ->groupBy('program_kegiatan_r_k_a_s.nama', 'program_kegiatan_r_k_a_s.deskripsi', 'program_kegiatan_r_k_a_s.output')
+        ->where("program_kegiatan_r_k_a_s.tahun", "=", $year)
+        ->get();
+
+        return response()->json($result);
     }
 }
