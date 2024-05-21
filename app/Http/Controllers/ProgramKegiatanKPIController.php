c8c8cecbf2d9592136d60dd84a5d34ec51cc6667
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-05-21 10:07:36 +0700

    make file

---
+++ app/Http/Controllers/ProgramKegiatanKPIController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProgramKegiatanKPI;
+use App\Http\Requests\StoreProgramKegiatanKPIRequest;
+use App\Http\Requests\UpdateProgramKegiatanKPIRequest;
+
+class ProgramKegiatanKPIController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProgramKegiatanKPIRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProgramKegiatanKPI $programKegiatanKPI)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProgramKegiatanKPIRequest $request, ProgramKegiatanKPI $programKegiatanKPI)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProgramKegiatanKPI $programKegiatanKPI)
+    {
+        //
+    }
+}
