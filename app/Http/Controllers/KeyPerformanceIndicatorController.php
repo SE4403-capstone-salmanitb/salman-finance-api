f9ec49b1ad1a1d98828f4ece02209771efd66a2f
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-05-21 12:37:44 +0700

    make file for kpi

---
+++ app/Http/Controllers/KeyPerformanceIndicatorController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KeyPerformanceIndicator;
+use App\Http\Requests\StoreKeyPerformanceIndicatorRequest;
+use App\Http\Requests\UpdateKeyPerformanceIndicatorRequest;
+
+class KeyPerformanceIndicatorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKeyPerformanceIndicatorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KeyPerformanceIndicator $keyPerformanceIndicator)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKeyPerformanceIndicatorRequest $request, KeyPerformanceIndicator $keyPerformanceIndicator)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KeyPerformanceIndicator $keyPerformanceIndicator)
+    {
+        //
+    }
+}
