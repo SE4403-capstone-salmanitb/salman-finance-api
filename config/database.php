dac934a804ff33a0af748cf50a7f59331e23c4d9
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-05-29 16:38:19 +0700

    redis cache

--- config/database.php
+++ config/database.php
@@ -145,6 +145,8 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'scheme' => env('REDIS_SCHEME', 'tls'),
+
         ],
 
         'default' => [
@@ -154,6 +156,8 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'scheme' => env('REDIS_SCHEME', 'tls'),
+
         ],
 
         'cache' => [
@@ -163,6 +167,7 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+            'scheme' => env('REDIS_SCHEME', 'tls'),
         ],
 
     ],
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,8 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/login', function(){
+    return redirect('https://localhost/login', secure:true);
+});
+
 require __DIR__.'/auth.php';
