ed5be347d3058112a76655c74c939518dc62129f
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-06-04 16:20:22 +0700

    add geoip driver

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,20 @@ use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\ProgramKegiatanKPIController;
 use App\Http\Controllers\ProgramKegiatanRKAController;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Stevebauman\Location\Facades\Location;
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/myip', function (Request $request ){
+    if( $position = Location::get() ) {
+        return response(['ip'=> $position, 'message'=> 'success']);
+    } else {
+        return response(['ip'=> 'null', 'message'=> 'failed'], 500);
+    }
 });
 
 //Route::apiResource('/program', App\Http\Controllers\ProgramController::class)
