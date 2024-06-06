932593dd73297200373ca426674640211680e6be
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-06-06 16:56:18 +0700

    make the route and migration

--- database/migrations/2024_06_06_092602_create_laporan_bulanans_table.php
+++ database/migrations/2024_06_06_092602_create_laporan_bulanans_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('laporan_bulanans', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nama');
+            $table->string('kode');
+            $table->date('bulanLaporan');
+            $table->foreignId('disusun_oleh')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('diperiksa_oleh')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamp('tanggal_pemeriksaan')->nullable();
         });
     }
 
