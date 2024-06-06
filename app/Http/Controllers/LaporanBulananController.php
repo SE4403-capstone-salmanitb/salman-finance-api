c599482428876d1851e3ddbbfe08c524a4b7547b
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-06-06 16:38:25 +0700

    make file for laporan bulanan

---
+++ app/Http/Controllers/LaporanBulananController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LaporanBulanan;
+use App\Http\Requests\StoreLaporanBulananRequest;
+use App\Http\Requests\UpdateLaporanBulananRequest;
+
+class LaporanBulananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLaporanBulananRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LaporanBulanan $laporanBulanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLaporanBulananRequest $request, LaporanBulanan $laporanBulanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LaporanBulanan $laporanBulanan)
+    {
+        //
+    }
+}
