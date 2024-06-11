de7b386699f1048d9f725db631a82ba0d1ad8167
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-06-11 12:11:51 +0700

    initial commit for pelaksanaan

---
+++ app/Http/Controllers/PelaksanaanController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pelaksanaan;
+use App\Http\Requests\StorePelaksanaanRequest;
+use App\Http\Requests\UpdatePelaksanaanRequest;
+
+class PelaksanaanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePelaksanaanRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pelaksanaan $pelaksanaan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePelaksanaanRequest $request, Pelaksanaan $pelaksanaan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pelaksanaan $pelaksanaan)
+    {
+        //
+    }
+}
