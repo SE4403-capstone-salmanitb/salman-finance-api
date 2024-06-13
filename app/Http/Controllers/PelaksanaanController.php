e6e9264f6e675f138eae5c6f92619343e8e677e3
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-06-13 10:13:36 +0700

    pelaksanaan controler func index

--- app/Http/Controllers/PelaksanaanController.php
+++ app/Http/Controllers/PelaksanaanController.php
@@ -5,15 +5,45 @@ namespace App\Http\Controllers;
 use App\Models\Pelaksanaan;
 use App\Http\Requests\StorePelaksanaanRequest;
 use App\Http\Requests\UpdatePelaksanaanRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Request;
 
 class PelaksanaanController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        Gate::authorize("viewAny", Pelaksanaan::class);
+        $query = DB::table('pelaksanaans');
+
+        $likeFilters = [
+            "penjelasan",
+            "waktu",
+            "tempat",
+            "penyaluran",
+        ];
+
+        $idFilters = [
+            "id_program_kegiatan_kpi",
+            "id_laporan_bulanan"
+        ];
+
+        foreach ($idFilters as $key) {
+            if ($request->has($key)){
+                $query->where($key, '=', $request->input($key));
+            }
+        }
+
+        foreach ($likeFilters as $key) {
+            if ($request->has($key)){
+                $query->where($key, 'like', '%'.$request->input($key).'%');
+            }
+        }
+
+        return response()->json($query->get());
     }
 
     /**
