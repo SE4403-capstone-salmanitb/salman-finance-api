6fecd6f71ac79cb7c151e63a5f058d044f0534eb
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-07-02 14:35:50 +0700

    file for alokasi dana

---
+++ app/Http/Controllers/AlokasiDanaController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AlokasiDana;
+use App\Http\Requests\StoreAlokasiDanaRequest;
+use App\Http\Requests\UpdateAlokasiDanaRequest;
+
+class AlokasiDanaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAlokasiDanaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AlokasiDana $alokasiDana)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAlokasiDanaRequest $request, AlokasiDana $alokasiDana)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AlokasiDana $alokasiDana)
+    {
+        //
+    }
+}
