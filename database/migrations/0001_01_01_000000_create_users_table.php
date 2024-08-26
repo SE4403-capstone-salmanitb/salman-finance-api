125e189a10f47ebf69ca5caba2adf6b17f9f5e79
AdMFirst <90541480+AdMFirst@users.noreply.github.com>
2024-08-26 16:32:47 +0700

    tambah role baru

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_admin')->default(false);
+            $table->boolean('is_mobile_user')->default(false);
             $table->string('password');
             $table->string("profile_picture")->nullable()->default("https://i.ibb.co.com/2qXwjX3/default-blue1.png");
             $table->rememberToken();
